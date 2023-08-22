8c5b09e27432bc8169dc49a600146bad30b08831
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-22 14:46:37 +0200

    Add project files.

---
+++ ExercisePetle3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ExercisePetle2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Calculation
+
+            int a = 0;
+            int b = 1;
+
+            do
+            {
+                Console.WriteLine(a);
+                b = a + b;
+                a = b - a;
+            }
+            while (b>0);
+        }
+    }
+}
